d4171098590e45690fdb22447158a0326912b85d
unknown <taremautsa@yahoo.com>
2020-06-16 23:00:26 +0200

    Unit Tests Functional

--- src/app/share/share.page.spec.ts
+++ src/app/share/share.page.spec.ts
@@ -1,11 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, AlertController, LoadingController } from '@ionic/angular';
 
 import { SharePage } from './share.page';
+import { MapModalComponent } from '../shared/map-modal/map-modal.component';
+import { Renderer2 } from '@angular/core';
 
-describe('SharePage', () => {
-  let component: SharePage;
-  let fixture: ComponentFixture<SharePage>;
+describe('SharePage Tests', () => {
+  let share: SharePage;
+  let map: MapModalComponent
+  let renderer: Renderer2;
+  let alert: AlertController;
+  let load: LoadingController;
+  let spy: any;
 
   /*beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,11 +27,38 @@ describe('SharePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });*/
+  beforeEach(() => {
+    alert = new AlertController();
+    load = new LoadingController;
+    map = new MapModalComponent(renderer,alert,load);
+    share = new SharePage(map);
+  });
+
+  afterEach(() => {
+    alert =null;
+    load = null;
+    map = null;
+    share = null;
+  });
+
   it('Share Page Loads', () => {
-    expect(2+2).toBe(4);
+    expect(SharePage).toBeDefined();
   });
   it('Testing Share location via email', () => {
-    expect(2+6).toBe(8);
+   spy = spyOn(share, 'currentCoordinates');
+   share.currentCoordinates();
+   expect(spy).toHaveBeenCalled();
   });
+
+  /*it('Testing Landmark location via email', () => {
+    spy = spyOn(share, 'landmarkLocation');
+    share.landmarkLocation();
+    expect(spy).toHaveBeenCalled();
+   });
   
+   it('Testing Event location via email', () => {
+    spy = spyOn(share, 'eventLocation');
+    share.eventLocation();
+    expect(spy).toHaveBeenCalled();
+   });*/
 });
