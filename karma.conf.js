df8e517c7b2978ec773667b04eab6d7317e23e11
Tcoco4 <36608817+Tcoco4@users.noreply.github.com>
2020-07-16 18:50:19 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeCanary', 'Firefox', 'Opera', 'IE', 'ChromeHeadless'],
+    browsers: ['Chrome', 'Firefox', 'Opera', 'IE', 'ChromeHeadless'],
     singleRun: false
   });
 };
